a96204c9e23c463333e0c71475dc78cd759f3af2
Mark Morgan <github@mark-morgan.com>
2021-02-20 18:12:40 +0000

    fixed license message on interface to make MIT

--- src/Filter/I.php
+++ src/Filter/I.php
@@ -8,13 +8,10 @@
  * @author District5 Digital
  * @link https://www.district5.co.uk
  *
- * @license This software and associated documentation (the "Software") may not be
- * used, copied, modified, distributed, published or licensed to any 3rd party
- * without the written permission of District5 or its author.
+ * @license MIT
  *
  * The above copyright notice and this permission notice shall be included in
  * all licensed copies of the Software.
- *
  */
 namespace District5\Filter;
 
